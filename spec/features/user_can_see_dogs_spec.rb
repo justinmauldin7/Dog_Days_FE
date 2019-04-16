af84089486c7d68ae72fcba381b3f302ca900e97
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 15:47:06 -0600

    Updates show dogs test to assert image is showing for a dog.

--- spec/features/user_can_see_dogs_spec.rb
+++ spec/features/user_can_see_dogs_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-describe "user can search for house members" do
-  it "user submits valid state name", :vcr do
+describe "on the root page" do
+  it "can see a list of dogs", :vcr do
     visit '/'
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_css(".dog", count: 3)
+    expect(page).to have_css(".dog", count: 6)
 
     within(first(".dog")) do
-      expect(page).to have_content("Bulldog")
+      expect(page).to have_content("Labrador")
+      expect(page.find('#dog_image')['src']).to have_content 'https://images.dog.ceo/breeds/labrador/n02099712_6901.jpg'
     end
   end
 end
