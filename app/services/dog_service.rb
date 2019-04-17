bd3c1b017ffdfae29996ed4b433ab1bb1f30fd24
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 18:59:00 -0600

    Creates method to create a dog in the BE API.

--- app/services/dog_service.rb
+++ app/services/dog_service.rb
@@ -3,4 +3,9 @@ class DogService
     response = Faraday.get 'https://dog-days-api.herokuapp.com/api/v1/dogs.json'
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.create_dog(breed, image)
+    response = Faraday.post "https://dog-days-api.herokuapp.com/api/v1/dogs?breed=#{breed}&image=#{image}"
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
