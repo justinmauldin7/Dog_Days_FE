a08a268dc69967f9247537c480625fc4ba0d9e16
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-17 02:15:56 -0600

    Adds attempt at new, create & destroy methods in the sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,25 @@
 class SessionsController < ApplicationController
   def new
-    if @current_user
+    binding.pry
+    if current_user
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def create
+    binding.pry
+    user = UserService.find_user(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:email] = user[:email]
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
   end
 end
