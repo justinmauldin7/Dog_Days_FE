8c388e261e7845c12e6a0d89e623ff1a9ee78ee4
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-16 18:58:41 -0600

    Adds create method for favorite.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,4 +2,11 @@ class FavoritesController < ApplicationController
   def index
     @dogs = DogService.get_all_dogs
   end
+
+  def create
+    @dog = DogService.create_dog(params[:breed], params[:image])
+    if @dog
+      redirect_to favorites_path
+    end
+  end
 end
