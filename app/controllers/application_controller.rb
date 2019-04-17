a63a29987ba52f4711dd65b1d1fef4bee01828f1
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-17 02:16:16 -0600

    Adds current_user helper method to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    binding.pry
+    @current_user ||= UserService.find_user(session[:email]) if session[:email]
+  end
 end
