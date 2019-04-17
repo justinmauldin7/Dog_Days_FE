1b68255ff4391aa14f1a3869924d8a94bcdc9e4a
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-17 00:19:00 -0600

    Adds test for user logging in.

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'As a User' do
+  it 'can login as a user' do
+    email = "justin@gmail.com"
+    password = "123"
+
+    visit root_path
+    click_link "Login"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :email, with: email
+    fill_in :password, with: password
+
+    click_button "Login"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Welcome, #{email}")
+    expect(page).to_not have_link("Login")
+    expect(page).to have_link("Logout")
+  end
+end
